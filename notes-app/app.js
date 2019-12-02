e0e5037393e9cbba3b0029ecb83b249a6e17f97b
Carlos H. Antunes <h10antunes@gmail.com>
2019-12-02 09:31:06 -0300

    Start

---
+++ notes-app/app.js
@@ -0,0 +1,5 @@
+const check = require('./utils');
+// const fs = require('fs');
+// fs.writeFileSync('notes.txt', 'this file was created by nodejs.');
+// fs.appendFileSync('notes.txt', "Add more notes here");
+check()
---
+++ notes-app/utils.js
@@ -0,0 +1,5 @@
+const check = function () {
+    console.log("Inside of utils.js");
+}
+
+module.exports = check
